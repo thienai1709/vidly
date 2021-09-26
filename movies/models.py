7f4daaca0515248332ca6e076eaf2d4bc116c39e
thienai1709 <thienaiho95@gmail.com>
2021-09-26 21:35:44 +0700

    Create models of movies module.

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Movie(modes.Model):
+    title = models.CharField(max_length=255)
+    released_year = models.IntegerField()
+    number_in_stock = models.IntegerField()
+    daily_rate = models.FloatField()
+    genre = models.ForeignKey(to=Genre, on_delete=models.CASCADE)
